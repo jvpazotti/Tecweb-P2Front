cbefd45146b54ab5b9923ef70f7a02cfe6651d0e
matkwon <kwonmat@outlook.com.br>
2021-11-25 20:40:43 +0900

    Update index.js

--- FrontEnd/src/components/Home/index.js
+++ FrontEnd/src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import Search from "../Search";
 import {
@@ -7,9 +7,26 @@ import {
     Route,
     Link
   } from "react-router-dom";
+import axios from "axios";
   
 
 export default function Home(){
+
+    const [msg, setMsg] = useState("");
+    const [token, setToken] = useState("");
+
+    const Generate = () => {
+
+      axios.get("https://enigmatic-bayou-56424.herokuapp.com/matheusk/token")
+        .then((response) => {
+
+          setToken(response.data.token);
+          
+          axios.post("https://enigmatic-bayou-56424.herokuapp.com/matheusk/message", {"token": token})
+            .then((response) => setMsg(response.data.mensagem))
+
+        })
+    }
 
     return(
 
@@ -19,6 +36,10 @@ export default function Home(){
         <div className="header">
         <img src="/DUMMiOS.png" className="logo"/>
         <p className="textao">Música para Leigos</p>
+        </div>
+        <div>
+          <button onClick={Generate}>Gerar Mensagem</button>
+          <h2>{msg}</h2>
         </div>
         <Link className="link" to="/favorites">
         <button className="button3">Músicas Favoritas</button>
